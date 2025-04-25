f60cad458a44f38dc5c448eed1499361a9439536
playersgate <yuji.sato@playersgate.jp>
2025-04-25 23:07:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ document.addEventListener("DOMContentLoaded", async () => {
       a.href = URL.createObjectURL(blob);
       
       const disposition = response.headers.get('Content-Disposition');
-      filename = employee + ".csv";//`${date.replaceAll("-", "")}${employee}.csv`;
+      filename =`特別休暇_${date.replaceAll("-", "")}_${employee}.csv`;
       if (disposition && disposition.includes('filename=')) {
         filename = decodeURIComponent(disposition.split("''")[1]);
       }
